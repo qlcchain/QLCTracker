ec543f12652da392b222d8363213d54b449cb118
zeriX <zero@x0.si>
2019-08-13 04:59:54 +0200

    update msg

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,6 +8,7 @@ import { WalletService } from './services/wallet.service';
 import { PriceService } from './services/price.service';
 import { AddressBookService } from './services/address-book.service';
 import { Router } from '@angular/router';
+import { ApiService } from './services/api.service';
 
 @Component({
   selector: 'app-root',
@@ -15,23 +16,28 @@ import { Router } from '@angular/router';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  wallet = this.walletService.wallet;
+	wallet = this.walletService.wallet;
+	showUpdate = false;
+	updateText = '';
+	updateLink = '';
+	updateBreak = 0;
   
-  constructor(
-    private walletService: WalletService,
-		private addressBook: AddressBookService,
-		public settings: AppSettingsService,
-		private notifications: NotificationService,
-		public nodeService: NodeService,
-		private router: Router,
-		private workPool: WorkPoolService,
-		public price: PriceService,
-		private lang: LangService
-  ) {
-  }
+	constructor(
+		private walletService: WalletService,
+			private addressBook: AddressBookService,
+			public settings: AppSettingsService,
+			private notifications: NotificationService,
+			public node: NodeService,
+			private router: Router,
+			private workPool: WorkPoolService,
+			public price: PriceService,
+			private lang: LangService,
+			private api: ApiService
+	) {
+	}
 
-  async ngOnInit() {
-    // When the page closes, determine if we should lock the wallet
+	async ngOnInit() {
+		// When the page closes, determine if we should lock the wallet
 		window.addEventListener('beforeunload', e => {
 			if (this.wallet.locked) {
 				return; // Already locked, nothing to worry about
@@ -48,10 +54,31 @@ export class AppComponent {
 				this.walletService.lockWallet();
 			}
 		});
-    await this.walletService.loadStoredWallet();
-    this.settings.loadAppSettings();
+		await this.walletService.loadStoredWallet();
+		this.settings.loadAppSettings();
 		this.addressBook.loadAddressBook();
 		this.workPool.loadWorkCache();
-  }
+
+		this.updates();
+	}
+
+	async updates() {
+		const updatesQuery = await this.api.updates();
+		if (updatesQuery.result && updatesQuery.result != '') {
+			this.showUpdate = true;
+			this.updateText = updatesQuery.result.title;
+			this.updateLink = updatesQuery.result.link;
+			this.updateBreak = updatesQuery.result.break;
+			if (this.updateBreak == 1) {
+				this.node.break = true;
+			} else {
+				this.node.break = false;
+			}
+		}
+	}
+
+	closeUpdate() {
+		this.showUpdate = false;
+	}
   
 }
