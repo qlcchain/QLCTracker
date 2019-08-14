66ab22b28bdd715252c982fb2d7b7204d095be47
zeriX <zero@x0.si>
2019-08-14 02:32:12 +0200

    package change chmod to chmod-plus

--- download-node.js
+++ download-node.js
@@ -1,6 +1,6 @@
 const fs = require('fs-extra');
 const download = require('download');
-const chmod = require('chmod');
+const chmod = require('chmod-plus');
 
 const version = 'v1.2.4';
 const gitrev = '959bc98';
@@ -27,13 +27,13 @@ if (!fs.existsSync(dir)){
 console.log('download macOS x64 binary');
 download('https://github.com/qlcchain/go-qlc/releases/download/'+version+'/gqlc-'+version+'-'+gitrev+'-darwin-10.10-amd64').then(data => {
     fs.writeFileSync('extra/darwin/x64/gqlc', data);
-    chmod('extra/darwin/x64/gqlc',700);
+    chmod.file(700,'extra/darwin/x64/gqlc');
 });
 
 console.log('download macOS x64 binary test binary');
 download('https://github.com/qlcchain/go-qlc/releases/download/'+version+'/gqlct-'+version+'-'+gitrev+'-darwin-10.10-amd64').then(data => {
     fs.writeFileSync('extra/darwin/x64/gqlct', data);
-    chmod('extra/darwin/x64/gqlct',700);
+    chmod.file(700,'extra/darwin/x64/gqlct');
 });
 
 
@@ -45,11 +45,11 @@ if (!fs.existsSync(dir)){
 console.log('download linux x64 binary');
 download('https://github.com/qlcchain/go-qlc/releases/download/'+version+'/gqlc-'+version+'-'+gitrev+'-linux-amd64').then(data => {
     fs.writeFileSync('extra/linux/x64/gqlc', data);
-    chmod('extra/linux/x64/gqlc',700);
+    chmod.file(700,'extra/linux/x64/gqlc');
 });
 
 console.log('download linux x64 binary test binary');
 download('https://github.com/qlcchain/go-qlc/releases/download/'+version+'/gqlct-'+version+'-'+gitrev+'-linux-amd64').then(data => {
     fs.writeFileSync('extra/linux/x64/gqlct', data);
-    chmod('extra/linux/x64/gqlct',700);
+    chmod.file(700,'extra/linux/x64/gqlct');
 });
